2f5d70cd8491b3e5181930f9e34886e4eb59ed20
Azat Yamalov <yamalov.azat@live.com>
2022-11-15 10:36:27 +0300

    new Tabs

--- pl-tab.js
+++ pl-tab.js
@@ -4,22 +4,94 @@ class PlTab extends PlElement {
 	static properties = {
 		header: { type: String },
 		selected: { type: Boolean, reflectToAttribute: true },
-		hidden: { type: Boolean, reflectToAttribute: true }
+		hidden: { type: Boolean, reflectToAttribute: true },
+		disabled: { type: Boolean, reflectToAttribute: true }
 	}
 
 	static css = css`
+		:host {
+			display: flex;
+			flex-direction: column;
+			color: var(--grey-darkest);
+		}
+
 		:host([hidden]) {
 			display: none;
 		}
+
+		.header-text {
+			display: flex;
+			user-select: none;
+			flex-shrink: 0;
+		}
+
+		.header {
+            width: 100%;
+            height: 40px;
+            display: flex;
+            flex-direction: row;
+            gap: 8px;
+            box-sizing: border-box;
+            background: transparent;
+			align-items: center;
+			cursor: pointer;
+			position: relative;
+		}
+
+		.header::after {
+            content: "";
+            position: absolute;
+            bottom: 0;
+            left: 50%;
+            height: 2px;
+            width: 0%;
+            background: var(--primary-base);
+            transition: all 0.5s ease;
+        }
+
+		.tab-content {
+			display: none;
+			overflow: hidden;
+			width: 100%;
+			height: calc(100% - 48px);
+			position: absolute;
+			left: 0px;
+			top: 48px;
+		}
+
+		:host([disabled]) {
+			pointer-events: none;
+			color: var(--grey-dark);
+		}
+
+		:host([selected]) .tab-content {
+			display: flex;
+		}
+
+		:host([selected]) .header::after {
+            width: 100%;
+            content:'';
+            left: 0;
+        }
+
+		:host(:hover), :host([selected]) {
+            color: var(--primary-base);
+        }
 	`;
 
 	static template = html`
-		<slot></slot>
+		<div class="header" on-click="[[onSelectClick]]">
+			<slot name="prefix"></slot>
+			<div class="header-text">[[header]]</div>
+			<slot name="suffix"></slot>
+		</div>
+		<div class="tab-content">
+			<slot></slot>
+		</div>
 	`;
 
-	connectedCallback() {
-		super.connectedCallback();
-		this.dispatchEvent(new CustomEvent('pl-tab-change', {
+	onSelectClick() {
+		this.dispatchEvent(new CustomEvent('select-tab', {
 			bubbles: true
 		}));
 	}
--- pl-tabpanel.js
+++ pl-tabpanel.js
@@ -1,160 +1,58 @@
-import { PlElement, html, css } from "polylib";
+import { PlElement, html, css, } from "polylib";
 
 import './pl-tab.js';
 
 class PlTabPanel extends PlElement {
     static properties = {
-        _tabs: { type: Array, value: () => [] },
         selected: { type: Number, value: 0 }
     }
 
     static css = css`
         :host {
-            display: flex;
-            flex-direction: column;
-            width: 100%;
-            height: 100%;
-            box-sizing: border-box;
+			width: 100%;
+			height: 100%;
+			display: flex;
+			position: relative;
+            gap: 8px;
         }
 
-        .tab-header {
-            width: 100%;
-            height: 40px;
-            position: relative;
-            display: flex;
-            flex-direction: row;
-            gap: 8px;
-            box-sizing: border-box;
-            overflow: hidden;
-            flex-shrink:0;
-            background: transparent;
-            margin-bottom: 12px;
+        .line {
             border-bottom: 1px solid var(--grey-base);
-        }
-
-        .tab {
-            display: flex;
-            height: 100%;
-            align-items: center;
-            color: var(--grey-darkest);
-            user-select: none;
-            cursor: pointer;
-            font-size: 13px;
-            line-height: 16px;
-            flex-shrink:0;
-            padding:  0 4px;
-            gap: 8px;
-            position: relative;
-        }
-
-        .tab[hidden] {
-            display: none;
-        }
-
-        .suffix:empty, .prefix:empty {
-            display: none;
-        }
-
-        .tab[selected] {
-            color: var(--text-color);
-        }
-        
-        .tab:hover {
-            color: var(--primary-base);
-        }
-
-        .tab::after {
-            content: "";
+            width: 100%;
             position: absolute;
-            bottom: 0;
-            left: 50%;
-            height: 2px;
-            width: 0%;
-            background: var(--primary-base);
-            transition: all 0.5s ease;
-        }
-
-        .tab[selected]::after {
-            width: 100%;
-            content:'';
-            left: 0;
-        }
-
-        .content {
-            display: flex;
-            height: 100%;
-            width: 100%;
-            box-sizing: border-box;
-            overflow: auto;
-        }
-
-        .content ::slotted(*) {
-            width: 100%;
-            height: 100%;
-        }
-
-        .content ::slotted(:not([selected])) {
-            display: none;
+            top: 40px;
         }
     `;
 
     static template = html`
-        <div id="header" class="tab-header">
-            <template d:repeat="[[_tabs]]">
-                <div class="tab" hidden$="[[item.hidden]]" selected$="[[item.selected]]" on-click="[[onTabClick]]">
-                    <span>[[item.header]]</span>
-                </div>
-            </template>
-        </div>
-        <div class="content">
-            <slot></slot>
-        </div>
+        <div class="line"></div>
+        <slot></slot>
     `;
 
     connectedCallback() {
         super.connectedCallback();
-        this._boundNodeChanged = this._nodeChanged.bind(this);
-        this._observer = new MutationObserver(this._boundNodeChanged);
-        this.addEventListener('pl-tab-change', this._boundNodeChanged);
-        this._observer.observe(this, { childList: true });
-    }
+        this.addEventListener('select-tab', this.onSelectTab)
 
-    _nodeChanged() {
-        let isHiddenItem = false;
-
-        const tabs = Array.prototype.slice.call(this.querySelectorAll(':scope > pl-tab'))
-            .map((tab, idx) => {
-                if (tab.hidden && tab.selected) {
-                    isHiddenItem = true;
+        setTimeout(() => {
+            let tabs = Array.prototype.slice.call(this.querySelectorAll(':scope > pl-tab'));
+            tabs.forEach((tab, idx) => {
+                if (idx == this.selected) {
+                    tab.selected = true;
+                } else {
+                    tab.selected = false;
                 }
-                tab.selected = idx == 0;
-                return {
-                    selected: idx == 0,
-                    header: tab.header,
-                    hidden: tab.hidden,
-                    disabled: tab.disabled,
-                    node: tab
-                };
             });
+        }, 0)
 
-        tabs.find((item, index) => {
-            if (!item.hidden && isHiddenItem) {
-                item.selected = true;
-                this.selected = index;
-                return true;
-            }
-        });
-
-
-
-        this.set('_tabs', tabs);
     }
 
-    onTabClick(event) {
-        this._tabs.forEach((el, idx) => {
-            el.node.selected = el == event.model.item;
-            this.set(`_tabs.${idx}.selected`, el == event.model.item);
+    async onSelectTab(event) {
+        let tabs = Array.prototype.slice.call(this.querySelectorAll(':scope > pl-tab')).filter(x => x != event.target);
+        tabs.forEach(tab => {
+            tab.selected = false;
         });
+
+        event.target.selected = true;
     }
 }
 
